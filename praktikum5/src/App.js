a3c9b986295dd2ab0be7ce74a017028cb060831c
MSI <ivnka24@gmail.com>
2022-03-16 08:29:12 +0700

    praktikum2

--- praktikum5/src/App.js
+++ praktikum5/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,58 +7,39 @@ import {
   Link
 }  from "react-router-dom";
 
-export default function BasicExample() {
+export default function ParamsExample() {
   return (
     <Router>
       <div>
+        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/netflix">Netflix</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/gmail">Gmail</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/amazon">Amazon</Link>
           </li>
         </ul>
-        <hr />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
+          <Switch>
+              <Route path="/:id" children={<Child />} />
+          </Switch>
       </div>
     </Router>
   );
 }
 
-function Home() {
+function Child(){
+  let { id } = useParams();
+
   return (
     <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About () {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard(){
-  return(
-    <div>
-      <h2>Dashboard</h2>
+      <h3>ID: {id}</h3>
     </div>
   );
 }
