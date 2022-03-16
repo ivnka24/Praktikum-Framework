a001ca82909d4c887e16c196c86fa7b6ae3695f4
MSI <ivnka24@gmail.com>
2022-03-16 09:13:28 +0700

    use redirect

--- praktikum5/src/App.js
+++ praktikum5/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
+import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useRouteMatch } from "react-router-dom";
 import {
   BrowserRouter as Router,
@@ -8,75 +10,111 @@ import {
   Link
 }  from "react-router-dom";
 
-export default function NestingExample() {
+export default function AuthExample() {
   return (
     <Router>
       <div>
+        <AuthButton />
+
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/public">Public Page</Link>
           </li>
           <li>
-            <Link to="/topics">Topics</Link>
+            <Link to="/private">Private Page</Link>
           </li>
         </ul>
-        <hr />
-          <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/topics">
-                <Topics />
-              </Route>
-          </Switch>
-      </div>
+        
+        <Switch>
+          <Route path="/public">
+            <PublicPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/private">
+            <ProtectedPage />
+          </PrivateRoute>
+        </Switch>
+        </div>     
     </Router>
   );
 }
 
-function Home(){
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function AuthButton(){
+  let history = useHistory();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button
+      onClick={() => {
+        fakeAuth.signout(() => history.push("/"));
+      }}
+      >
+        Sign Out
+      </button>
+        </p>
+        ) : (
+          <p>You are not logged in.</p>
   );
 }
 
-function Topics(){
-  let { path,url } = useRouteMatch();
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
-        </li>
-        <li>
-          <Link to={`${url}/Wisata Alam, Museum`}>Travelling</Link>
-        </li>
-        <li>
-          <Link to={`${url}/Ibis, JW Marriot`}>Review Hotel</Link>
-        </li>
-      </ul>
-
-      <Switch>
-        <Route exact path={path}>
-          <h3>Please Select a Topic.</h3>
-        </Route>
-        <Route path={`${path}/:topicId`}>
-          <Topic />
-        </Route>
-      </Switch>
-    </div>
+function PrivateRoute({ children, ...rest }){
+  return(
+    <Route
+    {...rest}
+    render={({ location }) =>
+    fakeAuth.isAuthenticated ? (
+      children
+    ) : (
+      <Redirect 
+      to={{
+        pathname: "/login",
+        state: { from: location }
+      }}
+      />
+    )
+  }
+    />
   );
 }
 
-function Topic() {
-  let { topicId } = useParams();
+function PublicPage(){
+  return <h3>Public</h3>;
+}
 
-  return (
+function ProtectedPage() {
+  return <h3>Private</h3>
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return(
     <div>
-      <h3>{topicId}</h3>
+      <p>You Must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
     </div>
   );
-}
+};
