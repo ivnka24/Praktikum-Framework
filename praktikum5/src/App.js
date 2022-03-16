2a2c740266a5b91e5515607de391c616fc83d290
MSI <ivnka24@gmail.com>
2022-03-16 08:46:29 +0700

    praktikum nested

--- praktikum5/src/App.js
+++ praktikum5/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router";
+import { useRouteMatch } from "react-router-dom";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,39 +8,75 @@ import {
   Link
 }  from "react-router-dom";
 
-export default function ParamsExample() {
+export default function NestingExample() {
   return (
     <Router>
       <div>
-        <h2>Accounts</h2>
         <ul>
           <li>
-            <Link to="/netflix">Netflix</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/gmail">Gmail</Link>
-          </li>
-          <li>
-            <Link to="/yahoo">Yahoo</Link>
-          </li>
-          <li>
-            <Link to="/amazon">Amazon</Link>
+            <Link to="/topics">Topics</Link>
           </li>
         </ul>
+        <hr />
           <Switch>
-              <Route path="/:id" children={<Child />} />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/topics">
+                <Topics />
+              </Route>
           </Switch>
       </div>
     </Router>
   );
 }
 
-function Child(){
-  let { id } = useParams();
+function Home(){
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Topics(){
+  let { path,url } = useRouteMatch();
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Wisata Alam, Museum`}>Travelling</Link>
+        </li>
+        <li>
+          <Link to={`${url}/Ibis, JW Marriot`}>Review Hotel</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please Select a Topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
 
   return (
     <div>
-      <h3>ID: {id}</h3>
+      <h3>{topicId}</h3>
     </div>
   );
 }
