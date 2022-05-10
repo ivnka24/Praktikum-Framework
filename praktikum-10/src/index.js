67455a2a090a89fc6375debf7ca5ef110d714688
MSI <ivnka24@gmail.com>
2022-05-10 15:36:17 +0700

    praktikum 10 inite, make a file

---
+++ praktikum-10/src/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import routes from "./routes.js"
+import Header from "./Header"
+import './styles.css';
+
+export const AuthContext = React.createContext(null);
+
+function App() {
+    const [isLoggedIn, setLoggedIn] = useState(false);
+  
+    return (
+      <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+  
+        <div className="App">
+          <Router>
+            <Header />
+  
+            <p id="notif">
+              Is logged in? {JSON.stringify(isLoggedIn)}
+            </p>
+  
+            <Switch>
+              {routes.map(route => (
+                <Route key={route.path} path={route.path} exact={route.exact} component={route.main} />
+              ))}
+            </Switch>
+          </Router>
+        </div>
+      </AuthContext.Provider>
+    )
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
