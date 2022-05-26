9fd05da65f6c5df2a713b074d448c999c2e3eb5f
MSI <ivnka24@gmail.com>
2022-05-26 11:30:00 +0700

    fix

--- praktikum-12/src/firebase/config.js
+++ praktikum-12/src/firebase/config.js
@@ -1,11 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDo7xsflwjNasQqW_jaPFTCkiKx58Mp8N4",
     authDomain: "praktikum11-ccd20.firebaseapp.com",
+    databaseURL: "https://praktikum11-ccd20-default-rtdb.firebaseio.com",
     projectId: "praktikum11-ccd20",
     storageBucket: "praktikum11-ccd20.appspot.com",
     messagingSenderId: "201152436338",
     appId: "1:201152436338:web:afe32e810db783e947caeb",
     measurementId: "G-0F5G0LGWF5"
-};
+  };
 
 export default firebaseConfig;
