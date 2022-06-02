a6bf9a761e1b22e6f0289578a6e4411648484daa
MSI <ivnka24@gmail.com>
2022-06-02 21:16:25 +0700

    belom fix

---
+++ praktikum-13/src/App.js
@@ -0,0 +1,14 @@
+/*------------ App.js -------*/
+import React, { Component } from 'react';
+import "./App.css";
+import Student from './container/students'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Student />
+      </div>
+    );
+  }
+}
+export default App;
